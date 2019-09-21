a26c31bc726d442600b6e27c391fbf3195bf63af
Kike Mendez <luis17erebsamen@gmail.com>
2019-09-21 11:42:54 -0500

    Productos

--- app/src/main/java/com/example/chani/ubereats/MenuActivity.java
+++ app/src/main/java/com/example/chani/ubereats/MenuActivity.java
@@ -103,6 +103,7 @@ ListView lvtiendas;
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                     Intent intent = new Intent(MenuActivity.this, TiendaProductosActivity.class);
                     intent.putExtra("tienda", tiendas.get(i));
+                    startActivity(intent);
                 }
             });
         }
--- app/src/main/java/com/example/chani/ubereats/TiendaProductosActivity.java
+++ app/src/main/java/com/example/chani/ubereats/TiendaProductosActivity.java
@@ -1,14 +1,103 @@
 package com.example.chani.ubereats;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class TiendaProductosActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import modelos.Tienda;
+
+public class TiendaProductosActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    Tienda tienda = null;
+    TextView lblNombreTienda, lblDescripcionTienda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tienda_productos);
+        Intent intent = getIntent();
+        tienda = (Tienda) intent.getSerializableExtra("tienda");
+        Log.i("tienda", tienda.getNombre());
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap gmap) {
+        googleMap = gmap;
+        googleMap.setMinZoomPreference(17);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        LatLng ny = new LatLng(Float.parseFloat(tienda.getLatitud()), Float.parseFloat(tienda.getLongitud()));
+        googleMap.addMarker(new MarkerOptions().position(ny).title(tienda.getNombre()).visible(true));
+        //googleMap.getUiSettings().setScrollGesturesEnabled(false);
+        //googleMap.getUiSettings().setAllGesturesEnabled(false);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ny));
     }
 }
